6c2f5f561b25a693648a3299b50deec201634efc
thefulltimereader <thefulltimereader@gmail.com>
2011-05-28 03:37:52 -0400

    added static page and rspec

---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_server_session',
+  :secret      => '93e85ab187399f20ebe61ad2066c36cc7d9fd6b2ab3b56838f891e1c3415b802fc8ad39959f489218a9992afd829ad3696548244c05a17b6603ee91dc0db18e2'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
