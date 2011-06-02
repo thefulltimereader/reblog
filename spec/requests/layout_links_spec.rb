804c92e3f5837182ebe0afef5873512e367ea17d
thefulltimereader <thefulltimereader@gmail.com>
2011-06-02 14:29:56 -0400

    added layout routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get layout_links_index_path
-      response.status.should be(200)
-    end
-  end
 
   it "should have a HOME page at '/'" do
     get '/'
-    response.should have_selector('title', :content => 'Home')
+    response.should have_selector('title', :content => 'home')
+  end
+
+  it "should have a contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => 'contact')
   end
 
   it "should have a about page at '/about'" do
     get '/about'
-    response.should have_selector('title', :content => 'About')
+    response.should have_selector('title', :content => 'about')
   end
 end
