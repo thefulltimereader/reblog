8bfa0f2cc1d50a87fba8e7c2a2d9cbf2903ebed3
thefulltimereader <ajk377@moth.cs.nyu.edu>
2011-06-10 16:18:11 -0400

    after 258, can't make user correctly on console tho, check that out next

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :emal, :password, :password_confirmation
@@ -35,10 +35,19 @@ class User < ActiveRecord::Base
   private
    
      def encrypt_password
+       self.salt = make_salt if new_record?
        self.encrypted_password = encrypt(password)
      end
      
-     def encrypt
-       string #tmp
+     def encrypt(string)
+       secure_hash("#{salt}--#{string}")
+     end
+
+     def make_salt
+       secure_hash("#{Time.now.utc}--#{password}")
+     end
+
+     def secure_hash(string)
+       Digest::SHA2.hexdigest(string)<
      end
 end
